b7a6b671813b14fd3b535f3e381955939809c0d8
Zuza Babis <zuzanna.babis@gmail.com>
2019-11-24 18:01:25 +0100

    Add event listener to initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -64,12 +64,14 @@
 
       thisProduct.renderInMenu();
 
+      thisProduct.initAccordion();
+
       console.log('New Product:', thisProduct);
     }
 
     renderInMenu(){
 
-      const thisProduct= this;
+      const thisProduct = this;
 
       /* generate HTML based on template */
 
@@ -86,6 +88,45 @@
       /* add element to menu */
 
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    initAccordion(){
+
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickedElements = document.querySelectorAll(select.menuProduct.clickable);
+      console.log('click', clickedElements);
+
+      /* START: click event listener to trigger */
+
+      for(let clickedElement of clickedElements) {
+        clickedElement.addEventListener('click', function(){
+          console.log('clicked');
+          }); 
+        }
+
+        /* prevent default action for event */
+
+        /* toggle active class on element of thisProduct */
+
+        /* find all active products */
+
+        /* START LOOP: for each active product */
+
+          /* START: if the active product isn't the element of thisProduct */
+
+            /* remove class active for the active product */
+
+          /* END: if the active product isn't the element of thisProduct */
+
+        /* END LOOP: for each active product */
+
+      /* END: click event listener to trigger */
+    
+      
+
     }
   }
 
