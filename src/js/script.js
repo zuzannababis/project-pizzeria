d263db7a2a07c2b8282d8bd81feb9cde4a990c3f
Zuza Babis <zuzanna.babis@gmail.com>
2019-12-15 17:53:04 +0100

    Add initActions method

--- src/js/script.js
+++ src/js/script.js
@@ -292,12 +292,66 @@
   }
 
   class AmountWidget{
+
     constructor(element){
+
       const thisWidget = this;
+
+      thisWidget.getElements(element);
+
+      thisWidget.setValue(thisWidget.input.value);
+
+      thisWidget.initActions();
 
       console.log('AmountWidget', thisWidget);
       console.log('contructor arguments:', element);
     }
+
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+
+    }
+
+    setValue(value){
+
+      const thisWidget = this;
+
+      const newValue = parseInt(value);
+
+      /* TODO: Add validation */
+
+      thisWidget.value = newValue;
+
+      thisWidget.input.value = thisWidget.value;
+
+    }
+
+    initActions(){
+
+      const thisWidget = this;
+
+      thisWidget.input.addEventListener('change', function(){
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      thisWidget.linkDecrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+
+      thisWidget.linkIncrease.addEventListener('click', function(event){
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+
+    
+    }
+
   }
 
   const app = {
