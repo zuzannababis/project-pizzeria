c1fefeb3d01dca90748143d530ec730a592dd8e2
Zuza Babis <zuzanna.babis@gmail.com>
2020-01-05 17:29:01 +0100

    Start BaseWidget class

---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,14 @@
+class BaseWidget{
+    constructor(wrapperElement, initialValue){
+        const thisWidget = this;
+
+        thisWidget.dom = {};
+        thisWidget.dom.wrapper = wrapperElement;
+
+        thisWidget.value = initialValue;
+
+    }
+    
+}
+
+export default BaseWidget;
