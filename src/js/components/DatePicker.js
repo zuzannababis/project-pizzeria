9d77fbcd77eaaba1c4c4d39b0a60cdd8f1b7b026
Zuza Babis <zuzanna.babis@gmail.com>
2020-01-06 14:09:19 +0100

    Add initPlugin method

--- src/js/components/DatePicker.js
+++ src/js/components/DatePicker.js
@@ -1,5 +1,6 @@
 import BaseWidget from './BaseWidget.js';
-import {select, utils} from '../settings.js';
+import {settings, select, utils} from '../settings.js';
+import flatpickr from 'flatpickr';
 
 class DatePicker extends BaseWidget{
 
@@ -11,6 +12,30 @@ class DatePicker extends BaseWidget{
         thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.datePicker.input);
 
         thisWidget.initPlugin();
+    }
+
+    initPlugin(){
+        const thisWidget = this;
+
+        thisWidget.minDate = new Date(thisWidget.value);
+        thisWidget.maxDate = utils.addDays(thisWidget.minDate, settings.datePicker.maxDaysInFuture);
+
+        thisWidget.flatpickr(thisWidget.dom.input, {
+            defaultDate: thisWidget.minDate,
+            minDate: thisWidget.minDate,
+            maxDate: thisWidget.maxDate,
+            locale:{
+                firstDayOfWeek: 1
+            },
+            disable: [
+                function(date) {
+                    return (date.getDay() === 1);      
+                }
+            ],
+            onChange: function(dateStr){
+                thisWidget.value = dateStr;
+            },
+        });
     }
 
 }
