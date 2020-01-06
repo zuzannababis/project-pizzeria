431e9ad33b2150c34b4b1864d9138363e418a3c2
Zuza Babis <zuzanna.babis@gmail.com>
2020-01-06 19:05:43 +0100

    Start parseData and makeBooked methods

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -4,6 +4,7 @@ import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
 import {utils} from '../utils.js';
 
+
 class Booking {
 
     constructor(bookingWidget){
@@ -62,11 +63,40 @@ class Booking {
                 ]);
             })
             .then(function([bookings, eventsCurrent, eventsRepeat]){
-                console.log(bookings);
-                console.log(eventsCurrent);
-                console.log(eventsRepeat);
+                //console.log(bookings);
+                //console.log(eventsCurrent);
+                //console.log(eventsRepeat);
+                thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
             });
     }
+
+    parseData(bookings, eventsCurrent, eventsRepeat){
+        const thisBooking = this;
+
+        thisBooking.booked = {};
+
+        for(let item of eventsCurrent){
+            thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
+        }
+
+        console.log('thisBooking.booked', thisBooking.booked);
+    }
+
+    makeBooked(date, hour, duration, table){
+        const thisBooking = this;
+
+        if (typeof thisBooking.booked[date] == 'undefined'){
+            thisBooking.booked[date] = {}; 
+        }
+
+        const startHour = utils.hourToNumber(hour);
+
+        if (typeof thisBooking.booked[date][startHour] == 'undefined'){
+            thisBooking.booked[date][startHour] = []; 
+        }
+
+        thisBooking.booked[date][startHour].push(table);
+    } 
 
     render(bookingWidget){
         const thisBooking = this;
